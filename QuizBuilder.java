364b90a00f578021c98703959e95081ec5b2e33c
jcg0105 <juliagriner00@gmail.com>
2020-11-24 15:54:11 -0500

    Update QuizBuilder.java

--- QuizBuilder.java
+++ QuizBuilder.java
@@ -1,7 +1,43 @@
 /* file: Project/QuizBuilderAJAK.java */
 import java.util.*; import java.io.*;
 class QuizBuilder {
+	public static Scanner scan = new Scanner(System.in);
 	public static void main(String[] args) {
-		System.out.println("This is some code");
+		//test question, no files yet
+		Question q = new Question("What is the capital of Massachusetts?", "Amherst", "Boston", "Springfield", "Worchester", "Holyoke", "Boston");
+		System.out.println(q);
+		
 	}//main 
+	
+	static void read(String filename) {} //read file
+	static void write() {} //creates .csv file
+	static void checkAnswer(String guess) {}//can be either letter ("A") or whole answer ("B. Boston") 
+	
 }//class QuizBuilder 
+
+class Question {
+	//after file is read, use .split(,) to get question, choices (A-E), and answer
+	//currently the choices are set a strings so that the questions can have different answers,
+	//the content of them doesn't matter -- can this be done with ints instead? will be even easier handling
+	String question; String a; String b; String c; String d; String e; String ans; 
+	public Question(String q, String a, String b, String c, String d, String e, String ans) {
+		this.question = q; 
+		this.a = a; 
+		this.b = b; 
+		this.c = c; 
+		this.d = d; 
+		this.e = e; 
+		this.ans = ans;
+	} //constructor, create new Question Object 
+	//		GETTERS
+	public String getA() { return a; } 
+	public String getB() { return b; }
+	public String getC() { return c; }
+	public String getD() { return d; } 
+	public String getE() { return e; }
+	public String getAnswer() { return ans; }
+	public String toString() {
+		return question + "\nA. " + a + "\nB. " + b + "\nC. " + c + "\nD. " + d + "\nE. " + e;
+	}//toString 
+	
+}//class Question 
