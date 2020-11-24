63e7a9d96f1a0860df9c9e6387e78eae6e60d3d9
jcg0105 <juliagriner00@gmail.com>
2020-11-24 14:43:46 -0500

    Create QuizBuilder.java

---
+++ QuizBuilder.java
@@ -0,0 +1,7 @@
+/* file: Project/QuizBuilderAJAK.java */
+import java.util.*; import java.io.*;
+class QuizBuilder {
+	public static void main(String[] args) {
+		System.out.println("This is some code");
+	}//main 
+}//class QuizBuilder 
